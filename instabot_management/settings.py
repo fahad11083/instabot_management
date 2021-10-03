f815dc8d240acd4befa4c6de5d38ff3a089e44a6
fahad11083 <fahad.anwaar111@gmail.com>
2021-10-03 22:34:17 +0500

    add bootstrap

--- instabot_management/settings.py
+++ instabot_management/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard',
     'automation',
-    'rest_framework'
+    'rest_framework',
+    'crispy_forms'
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
